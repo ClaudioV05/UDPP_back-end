f252e652b8d71f214528a04c89918bc67a4dc5a3
Claudiomildo Ventura <claudiomildo@hotmail.com>
2024-12-28 22:16:02 -0300

    Was made a adjust and refactoring on UdppController.

--- src/main/java/com/udpp/app/presentation/controllers/UdppController.java
+++ src/main/java/com/udpp/app/presentation/controllers/UdppController.java
@@ -6,24 +6,77 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.udpp.app.presentation.dtos.DataDto;
 import com.udpp.app.presentation.dtos.MetaDataDto;
+import com.udpp.app.presentation.dtos.MetaTableDto;
 
+/**
+ * Controller Unified Development Power Platform - UDPP.
+ * <p>
+ * --
+ *
+ * @since 1.0
+ * @see
+ */
 @RestController
 @RequestMapping("udpp")
 public final class UdppController {
+
+	private static final String INFORMATION_PATH = "/information";
+	private static final String METADATA_PATH = "/metadata";
+	private static final String METATABLE_PATH = "/metatable";
 
 	@GetMapping(value = { "/udppInformation" })
 	public String udppInformation() {
 		return "Unified development platform";
 	}
-	
-	@PostMapping(value = { "/metadata" })
-	public MetaDataDto metadata(@RequestBody MetaDataDto dtoMetaData) {
-		
-		MetaDataDto resultDtoMetaData = new MetaDataDto();
-		resultDtoMetaData.setData(dtoMetaData.getData());
-		resultDtoMetaData.setArchitecture(dtoMetaData.getArchitecture());
 
-		return resultDtoMetaData;
+	@GetMapping(value = { INFORMATION_PATH })
+	public DataDto metaData() {
+
+		DataDto data = new DataDto();
+		return data;
+	}
+
+	/**
+	 * To receive the schema of database and do the creation of the tables with
+	 * their fields.
+	 *
+	 * @param string the string to be converted
+	 *               <p>
+	 *               --
+	 *
+	 * @since 1.0
+	 * @return
+	 * @see
+	 * @author Claudiomildo Ventura
+	 */
+	@PostMapping(value = { METADATA_PATH })
+	public MetaDataDto metaData(@RequestBody MetaDataDto metaData) {
+
+		MetaDataDto resultMetaData = new MetaDataDto();
+		resultMetaData.setData(metaData.getData());
+		resultMetaData.setArchitecture(metaData.getArchitecture());
+		return resultMetaData;
+	}
+
+	/**
+	 * To receive the table(s) with their field(s) that will generate the magic
+	 * solution - UDPP.
+	 * 
+	 * @param string the string to be converted
+	 *               <p>
+	 *               --
+	 *
+	 * @since 1.0
+	 * @return
+	 * @see
+	 * @author Claudiomildo Ventura
+	 */
+	@PostMapping(value = { METATABLE_PATH })
+	public MetaTableDto metaTable(@RequestBody MetaTableDto metaTable) {
+
+		MetaTableDto resultMetaTable = new MetaTableDto();
+		return resultMetaTable;
 	}
 }
---
+++ src/main/java/com/udpp/app/presentation/dtos/DataDto.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.udpp.app.presentation.dtos;
+
+/**
+ * 
+ */
+public class DataDto {
+
+	private String information;
+
+	public DataDto() {
+		this.information = "Unified Development Power Platform - UDP";
+	}
+
+	public String getInformation() {
+		return information;
+	}
+
+	public void setInformation(String information) {
+		this.information = information;
+	}
+}
