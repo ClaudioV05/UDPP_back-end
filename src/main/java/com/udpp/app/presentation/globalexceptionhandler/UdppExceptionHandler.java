351d83201189010049a97a41c99c07dcbd40eac9
Claudiomildo Ventura <claudiomildo@hotmail.com>
2024-12-29 23:25:24 -0300

    The new class UdppExceptionHandler was added.

---
+++ src/main/java/com/udpp/app/presentation/globalexceptionhandler/UdppExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.udpp.app.presentation.globalexceptionhandler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class UdppExceptionHandler extends ResponseEntityExceptionHandler {
+
+}
